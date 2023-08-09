5598ff4832ebf9f4258f273353bdf0694fa9b948
yinbing.liu <yinbing.liu@upai.com>
2023-08-09 11:05:38 +0800

    feat:init

--- packages/vitePlugin/src/index.ts
+++ packages/vitePlugin/src/index.ts
@@ -2,8 +2,8 @@
  * @Author: yinbing.liu
  * @Date: 2022-08-13 21:07:01
  * @LastEditors: yinbing.liu
- * @LastEditTime: 2023-08-08 16:02:03
- * @FilePath: /adi-pnpm-monorepo-main/packages/vitePlugin/src/index.ts
+ * @LastEditTime: 2023-08-08 17:25:57
+ * @FilePath: /cache-updat/packages/vitePlugin/src/index.ts
  * @Description: 
  */
 import getVersion from "@packages/core"
